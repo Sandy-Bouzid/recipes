3c5917e44798ec1414d2f2888ad0674a102e85af
Sandy-Bouzid <bouzid.sandy@gmail.com>
2024-03-11 11:10:28 +0100

    add basic templates

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -12,12 +12,18 @@ class RecipeController extends AbstractController
     #[Route('/recette', name: 'recipe.index')]
     public function index(Request $request): Response
     {
-        return new Response('Recettes');
+        return $this->render('recipe/index.html.twig');
     }
 
-    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements:['id'=>'\d+', 'slug'=>'[a-z0-9-]+'])]
+    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
     public function show(Request $request, string $slug, int $id): Response
     {
-        return new Response('Recette :' . $slug);
+        return $this->render(
+            'recipe/show.html.twig',
+            [
+                'slug' => $slug,
+                'id' => $id
+            ]
+        );
     }
 }
