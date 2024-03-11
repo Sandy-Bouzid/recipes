464d9e6dfdc2651f1c5aa52d6058e126fbe30ab1
Sandy-Bouzid <bouzid.sandy@gmail.com>
2024-03-11 09:55:46 +0100

    project install

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/recette', name: 'recipe.index')]
+    public function index(Request $request): Response
+    {
+        return new Response('Recettes');
+    }
+
+    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements:['id'=>'\d+', 'slug'=>'[a-z0-9-]+'])]
+    public function show(Request $request, string $slug, int $id): Response
+    {
+        return new Response('Recette :' . $slug);
+    }
+}
