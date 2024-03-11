bca761253fe8fa5ad1e1b6aa8626614293273247
Sandy-Bouzid <bouzid.sandy@gmail.com>
2024-03-11 16:55:28 +0100

    Recipe entity + repository

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Recipes
+
+- `composer install`
+- création du .env.local avec la DATABASE_URL :
+`DATABASE_URL="mysql://db_user:db_password@127.0.0.1:3306/db_name?serverVersion=8.0.31&charset=utf8mb4"`
+- recréation de la bdd : 
+`bin/console doctrine:database:create`
+- exécuter les migrations présentes dans le projet (s'il y en a) : 
+`bin/console doctrine:migrations:migrate`
--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,23 +12,30 @@ use Symfony\Component\Routing\Attribute\Route;
 class RecipeController extends AbstractController
 {
     #[Route('/recette', name: 'recipe.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, RecipeRepository $recipeRepository): Response
     {
-        return $this->render('recipe/index.html.twig');
+        $recipes = $recipeRepository->findAll();
+
+        $totalDuration = $recipeRepository->findTotalDuration();
+
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes,
+            'totalDuration' => $totalDuration
+        ]);
     }
 
     #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id): Response
+    public function show(Request $request, string $slug, int $id, RecipeRepository $recipeRepository): Response
     {
-        return $this->render(
-            'recipe/show.html.twig',
-            [
-                'slug' => $slug,
-                'id' => $id,
-                'test' => [
-                    'val1'=>'coucou',
+        $recipe = $recipeRepository->find($id);
+
+        if ($slug != $recipe->getSlug()) {
+            return $this->redirectToRoute('recipe.show', ['slug' => $recipe->getSlug(), 'id' => $id]);
+        }
+
+        return $this->render('recipe/show.html.twig', [
+                'recipe' => $recipe
                 ]
-            ]
         );
     }
 }
