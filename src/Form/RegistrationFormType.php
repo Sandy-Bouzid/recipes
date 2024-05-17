fcbedb720edd1e9da2b9ae72b1c5255ec194858e
Sandy-Bouzid <bouzid.sandy@gmail.com>
2024-05-17 09:46:29 +0200

    add connexion button to nav bar & toggle password

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -32,6 +32,7 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
+                'toggle' => true,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
